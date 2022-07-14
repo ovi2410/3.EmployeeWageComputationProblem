6630d21d558f08b8d7069eace62328dd17a13f08
ovi2410 <guneshwarifegade@gmail.com>
2022-07-14 09:06:18 +0530

    [ovi]Add:UC2_CalCulateDaliyEmployeeWage

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -1,19 +1,24 @@
-﻿using System;
-
-namespace employeeabsentpresent
+﻿class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        //constants
+        int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
+        int empHrs = 0;
+        int empWage = 0;
+        Random random = new Random();
+        int empCheck = random.Next(0, 2);
+        if (empCheck == IS_FULL_TIME)
         {
-            int IS_FULL_TIME = 1;
-            Random obj = new Random();
-            int empCheck = obj.Next(0,2);
-            if (empCheck == IS_FULL_TIME)
-                Console.WriteLine("employeee is present");
-            else
-                Console.WriteLine("employee is absent");
-            Console.ReadKey();
+            empHrs = 8;
+
         }
+        else
+        {
+            empHrs = 0;
+        }
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        Console.WriteLine("Emp Wage : " + empWage);
     }
-}
+ }
